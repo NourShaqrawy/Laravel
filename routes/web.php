c6b5dd6d2c3156621f5463136d9e6aec1f5a789f
NourShaqrawy <shkrawysaleh037@gmail.com>
2025-06-06 23:00:45 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\reC;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('f',[reC::class,'index']);
